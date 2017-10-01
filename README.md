39ed41a14200823e9aa47c97433692b20a61306d
caryoscelus <caryoscelus@gmx.com>
2017-10-02 00:48:57 +0300

    README & build script

---
+++ README.md
@@ -0,0 +1,30 @@
+RainyNite
+=========
+
+RainyNite is a free/libre software vector animation tool, currently in early
+development.
+
+This is a "meta-repository" containing all needed dependencies and parts as
+submodules. Its only purpose is to simplify building RainyNite for users.
+
+Building
+--------
+
+TODO: add detailed instructions
+
+You can use `update_and_build.sh` script to automatically update this repo and
+build RainyNite.
+
+External dependencies
+---------------------
+
+- [qt](https://www.qt.io/)
+- [Inkscape](https://inkscape.org/) - used to render SVG output of RainyNite "renderer"; can be replaced with other command line SVG renderer
+- [boost][http://boost.org]
+
+Build environment
+-----------------
+
+- git
+- cmake
+- ninja (used in `update_and_build.sh`, but you can replace it with `make`)
---
+++ update_and_build.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+BUILD_JOBS=-j1
+FMT_FLAGS="-DBUILD_SHARED_LIBS=ON"
+LIB2GEOM_FLAGS="-D2GEOM_BUILD_SHARED=ON -D2GEOM_CYTHON_BINDINGS=OFF -D2GEOM_BOOST_PYTHON=OFF -D2GEOM_TOYS=OFF -D2GEOM_USE_GPL_CODE=ON -D2GEOM_PERFORMANCE_TESTS=OFF"
+
+echo Usage: $0 "<cmake configure flags>"
+
+echo Updating...
+git pull
+git submodule update --init --recursive
+
+echo Build fmt...
+pushd fmt
+mkdir -p build
+cd build
+[[ ! -f CMakeCache.txt ]] && cmake -GNinja ${FMT_FLAGS} "$@" .. || cmake "$@" .
+ninja ${BUILD_JOBS} || exit 1
+ninja test
+sudo ninja -j1 install
+popd
+
+echo Building lib2geom...
+pushd lib2geom
+mkdir -p build
+cd build
+[[ ! -f CMakeCache.txt ]] && cmake -GNinja ${LIB2GEOM_FLAGS} "$@" .. || cmake "$@" .
+ninja ${BUILD_JOBS} || exit 1
+ninja test
+sudo ninja -j1 install
+popd
+
+echo Building rainy-morph...
+pushd morph
+mkdir -p build
+cd build
+[[ ! -f CMakeCache.txt ]] && cmake -GNinja "$@" .. || cmake "$@" .
+ninja ${BUILD_JOBS} || exit 1
+ninja test
+sudo ninja -j1 install
+popd
+
+echo Building core...
+pushd core
+mkdir -p build
+cd build
+[[ ! -f CMakeCache.txt ]] && cmake -GNinja "$@" .. || cmake "$@" .
+ninja ${BUILD_JOBS} || exit 1
+ninja test
+sudo ninja -j1 install
+popd
+
+echo Building studio...
+pushd studio
+mkdir -p build
+cd build
+[[ ! -f CMakeCache.txt ]] && cmake -GNinja "$@" .. || cmake "$@" .
+ninja ${BUILD_JOBS} || exit 1
+# ninja test
+# sudo ninja -j1 install
+popd
+
+echo All done!
+echo you can run studio/build/studio/rainynite-studio now
